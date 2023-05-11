45606e79ede1b6c6a8f8a027293e4a9098fe1a9e
Wellington Patriota <w_patriota@hotmail.com>
2023-05-11 20:07:06 -0300

    teste

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default async function IndexPage({
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
       <Title>Users</Title>
-      <Text>
+      <Text>Lista de usuarios
         A list of users retrieved from a MySQL database (PlanetScale).
       </Text>
       <Search />
