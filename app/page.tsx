b6658f63e02a16f95bc84256233aca445f11141b
Wellington Patriota <w_patriota@hotmail.com>
2023-05-31 19:38:00 -0300

    add team, sobre, back-video 2

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function IndexPage() {
                 href="#"
                 className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
               >
-                Iniciar
+                Conheça agora!
               </a>
               <a href="/welcome" className="text-sm font-semibold leading-6 text-white">
                 Saiba mais <span aria-hidden="true">→</span>
