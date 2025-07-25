52a2ef837e755f6dce38873a23fca63aec6a0d41
Wellington <w_patriota@hotmail.com>
2025-07-25 14:06:37 -0300

    prod

--- app/toast.tsx
+++ app/toast.tsx
@@ -8,7 +8,7 @@ export default function Toast() {
   return isHidden ? null : (
     <div className="sticky rounded-2xl w-11/12 sm:w-[581px] h-40 sm:h-[80px] p-0.5 z-10 bottom-10 left-0 right-0 mx-auto">
       <div className="rounded-[14px] w-full h-full bg-gray-50 border border-gray-200 flex flex-col sm:flex-row items-center justify-center sm:justify-between space-y-3 sm:space-y-0 px-5">
-        <p>donmenu</p>
+        <p>don menu</p>
       </div>
     </div>
   );
